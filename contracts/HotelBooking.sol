f11d4d1e93734b38a2ee0af722abaf01c21d15b1
cagla <152120131104@ogrenci.ogu.edu.tr>
2022-10-09 01:29:26 +0300

    refactoring

--- contracts/HotelBooking.sol
+++ contracts/HotelBooking.sol
@@ -2,33 +2,60 @@
 
 pragma solidity ^0.8.0;
 
+// Hotel Booking System Contract
+
+// This contract created for hotel owners and customer to manage their booking operations.
+
 contract HotelBooking {
-    
+
+// Room struct which contains room information like;
+// number (uint) : room number
+// price (uint): room price
+// available (bool) : the room available for booking or not by customers
+
      struct Room { 
          uint number; 
          uint price;
          bool available;
          }
 
+// owner of the contract (address payable) : hotel owner
     address payable public immutable owner;
 
+// DI with constructor
     constructor () {
         owner = payable(msg.sender);
     }
 
+// Rooms array created for holding rooms infos
          Room[] public rooms;
+
+// mapping customer address to room represents the book itself
          mapping(address => Room) public books;
 
+// Log event created for logging changes on chain
         event Log(string message, address indexed sender, uint price);
 
+
+// createRoom (func) : This function created for hotel owner to allows creating a room with the parameters which Room struct contains
+// Every created room's availablity assigned to true that means it is empty
+// Only owner of the hotel can create a room
         function createRoom(uint _num, uint _price) external OnlyOwner {
             rooms.push(Room(_num, _price, true));
         }
 
+// changeAvail (func) : This function created for changing room availablity and marked as internal for internal usage
         function changeAvail(uint _index, bool _avail) internal {
             rooms[_index].available = _avail; 
         }
 
+// book (func) : This function created for customers to book a room with the given parameters
+// _index : The index of the room in the rooms array
+
+// Sends room price to the owner from the sender
+// Changes the availablity of the room
+// Maping the customer address to the booked room
+// Emit the Log event  
         function book(uint _index) external payable Cost(_index) Avail(_index) {
             (bool sent, ) = owner.call{value: msg.value}("");
             require(sent, "Fail");
@@ -38,22 +65,26 @@ contract HotelBooking {
             emit Log("book", msg.sender, msg.value);
         }
 
+// complete (func) : This function created for completion of the booking process with the given parameters
+// _index : The index of the room which booked by the customer
+// Changes the availablity of the room
         function complete(uint _index) external {
             require(books[msg.sender].number == rooms[_index].number, "Not your book");
             delete books[msg.sender];
             changeAvail(_index, true);
         } 
 
+//  OnlyOwner (modifier) : This modifier created for checking the sender is owner
         modifier OnlyOwner {
             require(msg.sender == owner, "Not an owner");
             _;
         }
-
+//  Cost (modifier) : This modifier created for checking the room's price is equal to value prop of the sender for payment process
         modifier Cost(uint _index) {
             require(msg.value == rooms[_index].price, "Invalid value");
             _;
         }
-
+//  Avail (modifier) : This modifier created for checking the room's availablity for booking process
         modifier Avail(uint _index) {
             require(rooms[_index].available == true, "Room is full");
             _;
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,5 @@
+// HotelBooking contract is tested with the below functions using Mocha
+
 const HotelBooking = artifacts.require("HotelBooking");
 let instance;
 
@@ -6,6 +8,10 @@ before(async () => {
 });
 
 contract("HotelBooking", (accounts) => {
+  // 1. Create a new Room
+  // 2. Get the first element of the rooms array
+  // 3. Assert their values are the same
+
   it("New room should be reach from rooms array", async () => {
     await instance.createRoom(101, 1000);
     let room = await instance.rooms.call(0);
@@ -15,6 +21,10 @@ contract("HotelBooking", (accounts) => {
   });
 
   it("Should be revert: Not an owner", async () => {
+    // 1. Try to create a new Room with the account who is not an owner
+    // 2. Catch the error
+    // 3. Assert the error
+
     try {
       await instance.createRoom(268, 3000, { from: accounts[1] });
     } catch (err) {
@@ -23,11 +33,21 @@ contract("HotelBooking", (accounts) => {
   });
 
   it("Owner should be account[0]", async () => {
+    // 1. Get the owner from contract object
+    // 2. Assert addresses of the owner and accounts[0] are same
+
     let owner = await instance.owner();
     assert.equal(owner, accounts[0]);
   });
 
   it("Should booking infos save correctly", async () => {
+    // 1. Create a new Room
+    // 2. Get the room infos of the rooms array
+    // 3. Make a book with accounts[1] and a created room
+    // 4. Call this book via accounts[1] address
+    // 5. Compare the numbers & prices of the room and book objects
+    // 6. Assert the room availability is false
+
     await instance.createRoom(325, 6500);
     let room = await instance.rooms.call(1);
 
@@ -44,6 +64,11 @@ contract("HotelBooking", (accounts) => {
   });
 
   it("Should be revert: Not your book", async () => {
+    // 1. Create a new Room
+    // 2. Try to call complete function with the book which is not own the caller
+    // 3. Catch the error
+    // 4. Assert the error
+
     await instance.createRoom(386, 7600);
     try {
       await instance.complete(2, accounts[1], { from: accounts[0] });
@@ -53,6 +78,13 @@ contract("HotelBooking", (accounts) => {
   });
 
   it("Should be convert to room availability to true", async () => {
+    // 1. Create a new Room
+    // 2. Make a book with accounts[1] and a created room
+    // 3. Complete the booking process with calling complete function
+    // 4. Get the room and the book objects
+    // 5. Assert room availability must be true
+    // 6. Assert the book' props set to their default values
+
     await instance.createRoom(265, 3000);
 
     await instance.book(3, {
@@ -72,6 +104,12 @@ contract("HotelBooking", (accounts) => {
   });
 
   it("Should increase owner balance by room price", async () => {
+    // 1. Create a new Room
+    // 2. Get the balance of the owner
+    // 3. Make a book to this room
+    // 4. Get the new balance of the owner
+    // 5. Compare the balances via the room's price which is : 4500
+
     await instance.createRoom(502, 4500);
     let balance = await web3.eth.getBalance(accounts[0]);
 
@@ -82,7 +120,7 @@ contract("HotelBooking", (accounts) => {
 
     let newBalance = await web3.eth.getBalance(accounts[0]);
 
-    assert.equal(Number(balance) + 4500, Number(newBalance));
     assert.ok(newBalance > balance);
+    assert.equal(Number(balance) + 4500, Number(newBalance));
   });
 });
